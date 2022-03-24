e121f069be78b76df82604f8f6f17bc39f3e1357
120061203 <ccssll120061203@gmail.com>
2022-03-24 14:38:20 +0800

    swap2 3 4種寫法

--- ch7sortAndSearch/bubbleSort.cpp
+++ ch7sortAndSearch/bubbleSort.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+#define SWAP2(x, y, t) ((t)=(a), (a)=(b), (b)=(t))
 void swap(int *, int *);
 void bubbleSort(int[], int);
 int main()
@@ -8,11 +10,12 @@ int main()
     int originArr[] = {5, 7, 6, 4, 3, 8, 9};
     int originArr2[] = {3, 4, 5, 6, 7, 8, 9};
     int originArr3[] = {9, 8, 7, 6, 5, 4, 3};
-    // int a = 10;
-    // int b = 20;
-    // swap(&a, &b);
-    // printf("after swap a=%d\n", a);
-    // printf("after swap b=%d\n", b);
+    int a = 10;
+    int b = 20;
+    int t=0;
+    SWAP2(a, b,t);
+    printf("after swap a=%d\n", a);
+    printf("after swap b=%d\n", b);
 
     clock_t start_t, finish_t;
     long double total_t = 0;
@@ -34,6 +37,8 @@ int main()
     diff = finish_t - start_t; // ms
     printf("max to min worst sequence%f  ms\n", diff);
 
+
+
     system("pause");
     return 0;
 }
@@ -44,12 +49,38 @@ void swap(int *a, int *b)
     *a = *b;
     *b = temp;
 }
+struct temp_value
+{
+    int a;
+    int b;
+};
+typedef struct temp_value temp_value;
+temp_value swap4(int a, int b){
+    int temp =a;
+    a = b;
+    b = temp;
+    
+
+    temp_value result;
+    result.a =a;
+    result.b =b;
+    return result;
+    // (*temp_value).a
+    // temp_value->a
+}
+void swap3(int &a,int &b){
+    int temp =a;
+    a=temp;
+    temp =b;
+
+}
+
 void bubbleSort(int arr[], int len)
-{ //???ï¿??arr[1]~arr[n]
+{ //±Æ§Çarr[1]~arr[n]
     int count = 1;
     for (int i = 1; i <= len - 1; i++)
     {
-        int flag = 0; //flag ?????ï¿½swap??ï¿½ï¿½??
+        int flag = 0; //flag ªí¥Ü¬O§_swap
 
         for (int j = 0; j <= len - i - 1; j++)
         {
